55646b083a86850e1c9627ceacb4fd2f91ce6f62
Arnaja Ganguly <79324117+arnajaganguly123@users.noreply.github.com>
2023-09-08 15:51:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import FormComponent from './FormComponent';
-import OperationCodeComponent from './OperationCodeComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>React API Client</h1>
-      <FormComponent />
-      <OperationCodeComponent />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
